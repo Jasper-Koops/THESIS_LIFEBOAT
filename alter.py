a307a22aa37bd57781280c93253ede2b4138966b
Jasper Koops <jasperkoops91@gmail.com>
2017-05-26 13:45:29 +0200

    Removed redundant files
    - These files have been included in the main "scriptie_scraper" script

--- alter.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-data = sqlite3.connect("scraper_save_file.db")
-c = data.cursor()
-c.execute("ALTER TABLE saved_data ADD COLUMN 'document_text' TEXT")
-c.execute("ALTER TABLE saved_data ADD COLUMN 'pdf_url' TEXT")
-c.execute("ALTER TABLE saved_data ADD COLUMN 'enhanced_on' TEXT")
-
-#df = pd.read_sql_query("SELECT * FROM saved_data", data)
